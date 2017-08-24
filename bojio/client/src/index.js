19769abd0e7de0faf4a1f5c1fa2d6c27b1323492
Lai Cheng Yu <lai.cheng.yu.2012@vjc.sg>
2017-08-24 22:23:12 +0800

    bootstrap navbar

--- bojio/client/src/index.js
+++ bojio/client/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavBar from './NavBar';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavBar />, document.getElementById('root'));
 registerServiceWorker();
