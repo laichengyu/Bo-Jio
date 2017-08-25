3ba841cf27581a5d3a53cb05aa1e47ecadf5ab4f
Stefano Chiesa <base278@gmail.com>
2017-08-25 12:24:02 +0800

    Fix dev env

--- bojio/client/src/components/Login.js
+++ bojio/client/src/components/Login.js
@@ -10,12 +10,21 @@ class Login extends Component {
         <div className="Login-header">
           <img src={logo} className="Login-logo" alt="logo" />
           <h2>Don't say bojio!</h2>
-          <a href="/api/login">
+          <a href={this._getLoginUrl()}>
             <img src={fbButton} className="fb-login-button" alt="login-button"/>
           </a>
         </div>
       </div>
     );
+  }
+
+  _getLoginUrl() {
+    const env = process.env.NODE_ENV || "development";
+    if (env === 'production') {
+      return '/api/login';
+    } else {
+      return 'http://localhost:8081/api/login';
+    }
   }
 }
 
