bcad3e34fbf150c6acce08b64dc67dedd9f2d5f4
Stefano Chiesa <base278@gmail.com>
2017-08-29 15:45:47 +0800

    Reskin comments counter

--- bojio/client/src/components/EventBlock.js
+++ bojio/client/src/components/EventBlock.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Item, Icon, Label, List, Button, Divider } from 'semantic-ui-react';
+import { Item, Icon, Label, List, Divider } from 'semantic-ui-react';
 import FacebookProvider, { Comments, CommentsCount, Like } from 'react-facebook';
 import EventCreator from './EventCreator';
 import Facepile from './Facepile';
@@ -76,15 +76,15 @@ class EventBlock extends Component {
     const toggle = () => this.setState({ showDetails: !this.state.showDetails });
     return !this.state.showDetails
       ? (
-        <FacebookProvider appId={this.props.services.facebook.appId}>
-          <span
-            className='EventBlock-commentsCount'
-            onClick={toggle}>
-            <CommentsCount href={this._getUrl()} /> <Icon name='comment' />
-          </span>
-        </FacebookProvider>
+        <List divided horizontal>
+          <List.Item className="EventBlock-commentsCount" icon='comments' link onClick={toggle} content={
+             <FacebookProvider appId={this.props.services.facebook.appId}>
+                <CommentsCount href={this._getUrl()} />
+             </FacebookProvider>
+          } />
+        </List>
       )
-      : <span onClick={toggle}>Hide comments</span>;
+      : <span className="EventBlock-hideComments" onClick={toggle}>Hide comments</span>;
   }
 
   _maybeRenderDetails() {
