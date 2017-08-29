5be998b0523066a924a702121fde557e3ea9b4cd
Lai Cheng Yu <lai.cheng.yu.2012@vjc.sg>
2017-08-29 23:56:21 +0800

    basic createeventform page added

--- bojio/client/src/components/CreateEventForm.js
+++ bojio/client/src/components/CreateEventForm.js
@@ -1,12 +1,70 @@
 import React, { Component } from 'react';
 import './CreateEventForm.css';
+import { Label, Button, Header, Form } from 'semantic-ui-react';
 
 class CreateEventForm extends Component {
   render() {
     return (
-      
+      <div className="CreateEventForm">
+        <Header as='h2' id="CreateEventForm-intro">Jio your friends now!</Header>
+        <form className="ui form">
+          <div className="field">
+            <label>Title</label>
+            <input type="text" name="event-title" placeholder="Give it a short catchy name"></input>
+          </div>
+
+          <div className="field">
+            <label>Location</label>
+            <input type="text" name="location" placeholder="Specify where it's held"></input>
+          </div>
+
+          {/* Calendar and time script currently not working */}
+          <div className="field">
+            <div className="two fields">
+              <div className="field">
+                <label>Date</label>
+                <div className="ui calendar" id="example2">
+                  <div className="ui input left icon">
+                    <i className="calendar icon"></i>
+                    <input type="text" placeholder="Date"></input>
+                  </div>
+                </div>
+              </div>
+
+              <div className="five wide field">
+                <label>Time</label>
+                <div className="ui calendar" id="example3">
+                  <div className="ui input left icon">
+                    <i className="time icon"></i>
+                    <input type="text" placeholder="Time"></input>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label>Image/GIF</label>
+            <input type="text" name="image" placeholder="Add GIF/Image"></input>
+          </div>
+
+          <div className="field">
+            <label>Description</label>
+            <Form.TextArea rows="4" className="description"
+              placeholder="BYOB, Girlfriends not allowed, Late-comers treating. Set your rules here!">
+            </Form.TextArea>
+          </div>
+
+          <div className="field">
+            <Button  className="ui primary button" type="submit">Jio!</Button>
+            <Label className="left pointing label">
+              *Event will be created, and your friends will see it on their event feed!
+            </Label>
+          </div>
+        </form>
+      </div>
     );
   }
 }
 
-export createEventForm;
+export default CreateEventForm;
--- bojio/client/src/components/MainApp.js
+++ bojio/client/src/components/MainApp.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import EventList from './EventList';
 import './MainApp.css';
+import CreateEventForm from './CreateEventForm';
 
 class MainApp extends Component {
   render() {
     return (
       <div className="MainApp">
         <NavBar services={this.props.services} />
-        <EventList services={this.props.services} />
+        {/* <EventList services={this.props.services} /> */}
+        <CreateEventForm/>
       </div>
     );
   }
